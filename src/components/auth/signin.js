649798bd64afcf3460357dd140ac5f7505383793
Sean Bigler <seanbigler@mail.weber.edu>
2018-07-16 10:46:38 -0600

    Started on Header Wrapper and signup /signin pages

---
+++ src/components/auth/signin.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+
+class Signin extends Component
+{
+    render()
+    {
+        return (
+            <div className='sign-in'>
+                ...signin
+            </div>
+        )
+    }
+}
+
+export default Signin;
