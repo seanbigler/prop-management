cd678945d861071ca98f2df5c233cfbb42f2adc3
Sean Bigler <seanbigler@mail.weber.edu>
2018-07-17 08:53:29 -0600

    Refactored components

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -7,12 +7,12 @@ import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 import Signup from './components/auth/signup';
 import Signin from './components/auth/signin';
 
-import HeaderWrapper from './components/headerWrapper';
+import Layout from './components/layout';
 
 
 
@@ -21,10 +21,11 @@ function main() {
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <HeaderWrapper>
-            <Route path = '/' exact component={Signin}/>
+          <Layout>
+            <Route path = '/signin' exact component={Signin}/>
+            <Route path = '/' exact component={Signin}/>            
             <Route path = '/signup' component={Signup}/>
-          </HeaderWrapper>
+          </Layout>
 
         </Switch>
 
